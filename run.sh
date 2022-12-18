5126355ac3747c79117fe086b00cae3af05e4de5
Eapan11 <114228195+Eapan11@users.noreply.github.com>
2022-12-18 20:25:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,29 @@
-# GCN
-python trainer.py --model gcn --hidden_units 16 --lr 0.1 --dropout 0.0
-python trainer.py --model gcn --hidden_units 16 --lr 0.01 --dropout 0.0
-python trainer.py --model gcn --hidden_units 16 --lr 0.001 --dropout 0.0
+# GCN cora
+python trainer.py --model gcn --dataset cora --hidden_units 16 --lr 0.1 --dropout 0.0
+python trainer.py --model gcn --dataset cora --hidden_units 16 --lr 0.01 --dropout 0.0
+python trainer.py --model gcn --dataset cora --hidden_units 16 --lr 0.001 --dropout 0.0
 
-# GAT
-python trainer.py --model gat --hidden_units 16 --lr 0.1 --dropout 0.0 --heads 8
-python trainer.py --model gat --hidden_units 16 --lr 0.01 --dropout 0.0 --heads 8
-python trainer.py --model gat --hidden_units 16 --lr 0.001 --dropout 0.0 --heads 8
+# GAT cora
+python trainer.py --model gat --dataset cora --hidden_units 16 --lr 0.1 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset cora --hidden_units 16 --lr 0.01 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset cora --hidden_units 16 --lr 0.001 --dropout 0.0 --heads 8
 
+# GCN citeseer
+python trainer.py --model gcn --dataset citeseer --hidden_units 16 --lr 0.1 --dropout 0.0
+python trainer.py --model gcn --dataset citeseer --hidden_units 16 --lr 0.01 --dropout 0.0
+python trainer.py --model gcn --dataset citeseer --hidden_units 16 --lr 0.001 --dropout 0.0
+
+# GAT citeseer
+python trainer.py --model gat --dataset citeseer --hidden_units 16 --lr 0.1 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset citeseer --hidden_units 16 --lr 0.01 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset citeseer --hidden_units 16 --lr 0.001 --dropout 0.0 --heads 8
+
+# GCN pubmed
+python trainer.py --model gcn --dataset pubmed --hidden_units 16 --lr 0.1 --dropout 0.0
+python trainer.py --model gcn --dataset pubmed --hidden_units 16 --lr 0.01 --dropout 0.0
+python trainer.py --model gcn --dataset pubmed --hidden_units 16 --lr 0.001 --dropout 0.0
+
+# GAT pubmed
+python trainer.py --model gat --dataset pubmed --hidden_units 16 --lr 0.1 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset pubmed --hidden_units 16 --lr 0.01 --dropout 0.0 --heads 8
+python trainer.py --model gat --dataset pubmed --hidden_units 16 --lr 0.001 --dropout 0.0 --heads 8
