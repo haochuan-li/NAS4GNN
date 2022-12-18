32415ead1033bcebae04829f5385ac7a7ed2d9ad
Eapan11 <114228195+Eapan11@users.noreply.github.com>
2022-12-18 20:12:34 +0800

    Add files via upload

---
+++ datasets/PubmedDataset.py
@@ -0,0 +1,17 @@
+from dgl.data import PubmedGraphDataset
+
+class PubmedDataset:
+    def __init__(self, device):
+        self.dataset = PubmedGraphDataset()
+        self.num_classes = self.dataset.num_classes
+        self.device = device
+        
+    def load_cora_data(self):
+        
+        g = self.dataset[0].to(self.device)
+        features = g.ndata['feat']
+        labels = g.ndata['label']
+        train_mask = g.ndata['train_mask']
+        val_mask = g.ndata['val_mask']
+        test_mask = g.ndata['test_mask']
+        return g, features, labels, train_mask, val_mask, test_mask, self.num_classes
